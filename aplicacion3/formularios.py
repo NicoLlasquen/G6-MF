33940cf46c55154fe52206f82e0649803655b01f
Nico <nicollasquen@gmail.com>
2023-06-15 19:43:30 -0400

    agrege cosas

--- aplicacion3/formularios.py
+++ aplicacion3/formularios.py
@@ -4,10 +4,17 @@ from .models import Usuario, Comentarios
 
 class Crear_publicacion(forms.Form):
     titulo = forms.CharField(label="Titulo de la publicación", max_length=100)
-    texto = forms.Textarea()
+    texto = forms.CharField(
+        label="Contenido de la publicación", widget=forms.Textarea)
     usuario = Usuario.objects.all()
 
 
 class Crear_comentario(forms.Form):
-    texto = forms.Textarea()
+    texto = forms.CharField(label="Comentario", widget=forms.Textarea)
     usuario = Comentarios.objects.all()
+
+
+class Crear_usuario(forms.Form):
+    nombre = forms.CharField(label="Nombre", max_length=100)
+    rut = forms.IntegerField(label="Rut", max_value=8, min_value=8)
+    año = forms.CharField(label="Año académico", max_length=50)
